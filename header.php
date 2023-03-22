8c6efafc142db29af770ce201ba0b4d9da74db44
AmodPradhan <107186417+AmodPradhan@users.noreply.github.com>
2023-03-22 22:42:31 +0545

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+include('./config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sign.css">
+    <script src="https://kit.fontawesome.com/e29b4434c7.js" crossorigin="anonymous"></script>
+    <title>Dental App</title>
+</head>
+<body>
+    
